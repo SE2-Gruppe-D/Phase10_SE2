e081c39fa2d3118537786a447d08b176782faa75
Laura_LL <lauralik@edu.aau.at>
2022-04-09 21:02:37 +0200

    definierung des Buttons Spiel erstellen

--- app/src/main/java/com/example/phase10_se2/MainActivity.java
+++ app/src/main/java/com/example/phase10_se2/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.phase10_se2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button spielErstellen;
+    Button spielBeitreten;
+    Button hint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spielErstellen=findViewById(R.id.button2);
+        spielErstellen.setOnClickListener(view -> createGame());
+
+
+
+    }
+    public void createGame(){
     }
 }
