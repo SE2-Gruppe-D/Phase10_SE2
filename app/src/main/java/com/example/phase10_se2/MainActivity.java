3178916eb85c10ab86b3a83de90a48031172681f
Laura_LL <lauralik@edu.aau.at>
2022-04-10 13:01:35 +0200

    Anzeigen der Spielregeln im Hauptmenue mit dem Button HINT

--- app/src/main/java/com/example/phase10_se2/MainActivity.java
+++ app/src/main/java/com/example/phase10_se2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.phase10_se2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -22,10 +23,15 @@ public class MainActivity extends AppCompatActivity {
         spielBeitreten.setOnClickListener(view -> joinGame());
 
         hint=findViewById(R.id.button4);
+        hint.setOnClickListener(view -> showPlayRules());
 
     }
     public void createGame(){
     }
     public void joinGame() {
+    }
+    public void showPlayRules(){
+        Intent intent= new Intent(this, PlayRules.class);
+        startActivity(intent);
     }
 }
