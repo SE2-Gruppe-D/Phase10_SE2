aa05d931867ef48534592da22a8fb841ab1fc5f5
Melanie Hasler <melaniehas@edu.aau.at>
2022-04-14 17:53:23 +0200

    added class Phase

---
+++ app/src/main/java/com/example/phase10_se2/Phase.java
@@ -0,0 +1,19 @@
+package com.example.phase10_se2;
+
+public class Phase {
+
+    /*
+    Phase 1: 4 Zwillinge
+    Phase 2: 6 Karten einer Farbe
+    Phase 3: 1 Vierling + 1 Viererfolge
+    Phase 4: 1 Achterfolge
+    Phase 5: 7 Karten einer Farbe
+    Phase 6: 1 Neunerfolge
+    Phase 7: 2 Vierlinge
+    Phase 8: 1 Viererfolge einer Farbe + 1 Drilling
+    Phase 9: 1 Fünfling + 1 Drilling
+    Phase 10: 1 Fünfling + 1 Dreierfolge einer Farbe
+     */
+
+
+}
