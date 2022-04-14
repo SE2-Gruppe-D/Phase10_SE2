38845fcb8da0a232963552df7f2675724c8adb62
Melanie Hasler <melaniehas@edu.aau.at>
2022-04-14 17:43:16 +0200

    added class Card

---
+++ app/src/main/java/com/example/phase10_se2/Card.java
@@ -0,0 +1,21 @@
+package com.example.phase10_se2;
+
+public class Card {
+
+    private String color;
+    private int value;
+
+    public Card(String color, int value) {
+        this.color = color;
+        this.value = value;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+}
