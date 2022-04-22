9418a9446b1759da6f4a3dd0dfff926ff52aefbc
Theresa Marchetti <resa.marchetti@gmail.com>
2022-04-22 20:41:41 +0200

    gradle update

--- app/src/main/java/com/example/phase10_se2/Playfield.java
+++ app/src/main/java/com/example/phase10_se2/Playfield.java
@@ -1,18 +1,17 @@
 package com.example.phase10_se2;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
-
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
