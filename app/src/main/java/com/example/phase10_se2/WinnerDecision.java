edf6c7c4297c85e46172f75e5c3d0fad756e0e4c
TwisT8642 <baisan.andreas@gmail.com>
2022-05-09 11:45:45 +0200

    WinnerDecision almost done

--- app/src/main/java/com/example/phase10_se2/WinnerDecision.java
+++ app/src/main/java/com/example/phase10_se2/WinnerDecision.java
@@ -1,12 +1,16 @@
 package com.example.phase10_se2;
 
+import android.widget.Toast;
+
 import java.util.ArrayList;
 
-public class WinnerDecision {
+public class WinnerDecision
+{
     Player playerRed = null;
     Player playerBlue = null;
     Player playerYellow = null;
     Player playerGreen = null;
+    ArrayList<Player> actualPlayer;
 
     public WinnerDecision(ArrayList<Player> playerList)
     {
@@ -15,20 +19,56 @@ public class WinnerDecision {
             if (p.getColor().equals("RED"))
             {
                 playerRed = p;
+                actualPlayer.add(playerRed);
             }
             else if (p.getColor().equals("BLUE"))
             {
                 playerBlue = p;
+                actualPlayer.add(playerBlue;
             }
             else if (p.getColor().equals("YELLOW"))
             {
                 playerYellow = p;
+                actualPlayer.add(playerYellow);
             }
             else if (p.getColor().equals("GREEN"))
             {
                 playerGreen = p;
+                actualPlayer.add(playerGreen);
             }
         }
     }
-    
+    public PlayerColor getWinner()
+    {
+        int phaseCheck = 10;
+        int minusPointsCheck = -1;
+        Player temporaryWinner = null;
+        int Winnernumber = 0;
+        for (Player p : actualPlayer)
+        {
+            if(p.getPhasenumber() > phaseCheck && minusPointsCheck == -1)
+            {
+                minusPointsCheck=p.getMinusPoints();
+                temporaryWinner = p;
+            }
+            else if(p.getPhasenumber() > phaseCheck && p.getMinusPoints()<=minusPointsCheck)
+            {
+                if (minusPointsCheck == p.getMinusPoints())
+                {
+                    Winnernumber++;
+                }
+                minusPointsCheck=p.getMinusPoints();
+                temporaryWinner = p;
+            }
+        }
+        if (Winnernumber == 0)
+        {
+            return temporaryWinner.getColor();
+        }
+        else if (Winnernumber != 0)
+        {
+            
+        }
+            return temporaryWinner.getColor();
+    }
 }
