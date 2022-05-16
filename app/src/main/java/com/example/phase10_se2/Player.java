cc26d188dac89c709f47576883bd6ab7929044af
TwisT8642 <baisan.andreas@gmail.com>
2022-05-16 18:48:25 +0200

    phaseNumber übergabe Parameter in Player Konstruktor fixed

--- app/src/main/java/com/example/phase10_se2/Player.java
+++ app/src/main/java/com/example/phase10_se2/Player.java
@@ -77,7 +77,7 @@ public class Player {
         phaseText = "/";
     }
 
-    public Player(String name, PlayerColor color, int phasenumber) {
+    public Player(String name, PlayerColor color, int phaseNumber) {
         this.name = name;
         this.color = color;
         this.phaseNumber = phaseNumber;
