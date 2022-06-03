63c2e888725a24070abb9bca416298e2def037ff
Laura_LL <lauralik@edu.aau.at>
2022-06-03 19:29:00 +0200

    Tests fuer Visibility der Primary Player Handkarten

--- app/src/main/java/com/example/phase10_se2/CardsPrimaryPlayer.java
+++ app/src/main/java/com/example/phase10_se2/CardsPrimaryPlayer.java
@@ -5,8 +5,10 @@ import android.view.View;
 public class CardsPrimaryPlayer {
     //primaryPlayer soll nur seine Karten sehen
     public void showOnlyPrimaryPlayerCards(Player primaryPlayer){
-        for (Cards card : primaryPlayer.getPlayerHand()) {
-            card.getCardUI().setVisibility(View.VISIBLE);
+        if(primaryPlayer!=null){
+            for (Cards card : primaryPlayer.getPlayerHand()) {
+                card.getCardUI().setVisibility(View.VISIBLE);
+            }
         }
     }
 }
