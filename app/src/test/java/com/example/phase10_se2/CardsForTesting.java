d9282359e97f989a70fe36848645cc6904ea0c59
TwisT8642 <baisan.andreas@gmail.com>
2022-06-16 10:38:09 +0200

    Karten aus PhaseTest in eigene Klasse CardsForTesting ausgelagert

---
+++ app/src/test/java/com/example/phase10_se2/CardsForTesting.java
@@ -0,0 +1,37 @@
+package com.example.phase10_se2;
+
+public class CardsForTesting {
+
+        public Cards card8r = new Cards("RED", 8, null, 8);
+        public Cards card8g = new Cards("GREEN", 8, null, 32);
+        public Cards card7y = new Cards("YELLOW", 7, null, 7);
+        public Cards card7b = new Cards("BLUE", 7, null, 31);
+        public Cards card4r = new Cards("RED", 4, null, 4);
+        public Cards card4b = new Cards("BLUE", 4, null, 28);
+        public Cards card2g = new Cards("GREEN", 2, null, 2);
+        public Cards card2y = new Cards("YELLOW", 2, null, 26);
+        public Cards card10y = new Cards("YELLOW", 10, null, 10);
+        public Cards card1y = new Cards("YELLOW", 1, null, 1);
+        public Cards card5y = new Cards("YELLOW", 5, null, 5);
+        public Cards card12y = new Cards("YELLOW", 12, null, 12);
+        public Cards card11y = new Cards("YELLOW", 11, null, 11);
+        public Cards card3g = new Cards("GREEN", 3, null, 27);
+        public Cards card8y = new Cards("YELLOW", 8, null, 8);
+        public Cards card8b = new Cards("BLUE", 8, null, 32);
+        public Cards card6g = new Cards("GREEN", 6, null, 6);
+        public Cards card9r = new Cards("RED", 9, null, 9);
+        public Cards card7g = new Cards("GREEN", 7, null, 55);
+        public Cards card7r = new Cards("RED", 7, null, 79);
+        public Cards card3y = new Cards("YELLOW", 3, null, 333);
+        public Cards card4y = new Cards("YELLOW", 4, null, 44);
+        public Cards card2r = new Cards("RED", 2, null, 222);
+        public Cards card6y = new Cards("YELLOW", 6, null, 666);
+
+        public Cards card8b2 = new Cards("BLUE", 8, null, 64);
+        public Cards card8r2 = new Cards("RED", 8, null, 108);
+        public Cards card8g2 = new Cards("GREEN", 8, null, 116);
+        public Cards card8y2 = new Cards("YELLOW", 8, null, 124);
+        public Cards card2y2 = new Cards("YELLOW", 2, null, 82);
+
+
+}
