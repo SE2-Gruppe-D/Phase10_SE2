b6d5d071f7481dc8a86129af090e80f3a5b01676
GitHub-Leon <70367593+GitHub-Leon@users.noreply.github.com>
2022-06-21 13:23:32 +0200

    removed code smells
    removed code smells

--- app/src/main/java/com/example/phase10_se2/HandCards.java
+++ app/src/main/java/com/example/phase10_se2/HandCards.java
@@ -1,9 +1,5 @@
 package com.example.phase10_se2;
 
-import android.content.ClipData;
-import android.util.Log;
-import android.view.MotionEvent;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
